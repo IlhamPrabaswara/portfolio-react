d31caca1f09efe04b6564d11be7d20e08c3cf86b
Ilham Prabaswara <ilhamprabaswara@gmail.com>
2021-04-13 20:57:51 +0700

    Change homepage URL

--- src/App.js
+++ src/App.js
@@ -10,24 +10,6 @@ function App() {
       </button>
     )
   }
-  // class Square extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       value: null,
-  //     }
-  //   }
-  //   render() {
-  //     return (
-  //       <button
-  //         className="square"
-  //         onClick={() => this.props.onClick()}
-  //       >
-  //         {this.props.value}
-  //       </button>
-  //     );
-  //   }
-  // }
   class Board extends React.Component {
     constructor(props) {
       super(props);
@@ -38,6 +20,9 @@ function App() {
     }
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -54,7 +39,13 @@ function App() {
     }
 
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
       return (
         <div>
@@ -100,22 +91,22 @@ function App() {
 
 export default App;
 
-// function calculateWinner(squares) {
-//   const lines = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-//   ];
-//   for (let i = 0; i < lines.length; i++) {
-//     const [a, b, c] = lines[i];
-//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-//       return squares[a];
-//     }
-//   }
-//   return null;
-// }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
